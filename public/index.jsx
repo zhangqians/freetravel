3f9cd985ddbc312ff4e92472586b95c36ac02e5d
Leonard LiuTao <364125168@qq.com>
2016-08-13 11:29:52 +0800

    brief

--- public/index.jsx
+++ public/index.jsx
@@ -2,6 +2,7 @@
  * Created by Leonard on 16/8/13.
  */
 import React from 'react';
+import Brife from './brief/brizyn.jsx';
 
 
 export default class Index extends React.Component {
@@ -9,6 +10,9 @@ export default class Index extends React.Component {
         return (
             <div className="container-fluid">
                 hello
+                <div>
+                    <Brife/>
+                </div>
             </div>
         );
     }
