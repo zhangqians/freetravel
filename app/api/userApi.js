ce5affb6707fe944212fc927e29426137c213fbb
Leonard LiuTao <364125168@qq.com>
2016-08-17 13:28:53 +0800

    usersApi success 50%

--- app/api/userApi.js
+++ app/api/userApi.js
@@ -1,10 +1,35 @@
 import express from 'express';
+import {User} from '../db/schema';
+
 
 const router = express.Router();
 
 router.post('/', function (req, res, next) {
-    console.log('----api----');
+    console.log('----usersApi----');
+    let name = req.body.name;
+    let password = req.body.password;
+    let email = req.body.email;
+    let phone = req.body.phone;
+    User.findOne({name: name}, function (err, docs) {
+        if (err) res.send(err.message);
 
+        if (docs == null) {
+            console.log(name, password, email, phone);
+            var user = new User({
+                name: name,
+                password: password,
+                email: email,
+                phone: phone
+            });
+            user.save(function (err) {
+                console.log('save status:', err ? 'failed' : 'success');
+                res.send('success');
+            });
+        }
+        else {
+            res.send('error');
+        }
+    });
 
 });
 
