debe7d13d636b8ba52f6b53bc274788e2b5559bf
Leonard LiuTao <364125168@qq.com>
2016-08-17 12:47:45 +0800

    create userApi.js/update server.js/ and if I click register btn ,i can see--api--on terminal

---
+++ app/api/userApi.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/', function (req, res, next) {
+    console.log('----api----');
+
+
+});
+
+
+export default router;
