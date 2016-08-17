e4ea51ed3f3c83117ef34315ac10b78964869acd
Leonard LiuTao <364125168@qq.com>
2016-08-17 19:33:21 +0800

    register complete first commit

--- app/api/userApi.js
+++ app/api/userApi.js
@@ -4,65 +4,128 @@ import _ from 'lodash';
 
 
 const router = express.Router();
+//
+// function register(req, res, next) {
+//     const data = getUserInformation(req);
+//     console.log(data);
+//
+// }
 
-router.post('/', function (req, res, next) {
-    console.log('----usersApi----');
-    // const name = req.body.name;
-    // const password = req.body.password;
-    // const email = req.body.email;
-    // const phone = req.body.phone;
-    // const confirmPassword = req.body.confirmPassword;
+function getUserInformation(req) {
     const data = req.body;
+    return data;
+}
+
+function isUserInformationLegal(data) {
     const regEmail = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/;
     const regPhone = /^(\+86)?(1[0-9]{10})$/;
+
     console.log(regEmail.test(data.email));
     console.log(regPhone.test(data.phone) + 'Phone');
+
     if (data.name == '' || data.password == '' || data.email == '' || data.phone == '') {
         console.log(data);
         console.log('--data is null--');
-        // res.send('error').status(400);
-        res.status(400).send('Please finish the form');
+        // res.status(400).send('Please finish the form');
+        return {legal: false, message: 'Please finish the form'};
     }
-    else if (data.phone.length != 11 || regPhone.test(data.phone)==false) {
-        res.status(400).send('The phone number is error');
-    }
-    // else if(!data.email.includes('@')){
     else if (regEmail.test(data.email) == false) {
-        res.status(400).send('The email is error');
+        // res.status(400).send('The email is error');
+        return {legal: false, message: 'The email is error'};
     }
+    else if (data.phone.length != 11 || regPhone.test(data.phone) == false) {
+        // res.status(400).send('The phone number is error');
+        return {legal: false, message: 'The phone number is error'};
+    }
+    else return true;
+}
 
-    else {
+
+router.post('/', function (req, res, next) {
+    debugger;
+    getUserInformation(req);
+    const data = getUserInformation(req);
+    console.log('data:' + data.name);
+    const legal = isUserInformationLegal(data);
+    console.log('legal:' + legal);
+    if (legal == true) {
         User.findOne({name: data.name}, function (err, docs) {
-            // if (err) res.send(err.message);
             if (err) return next(err);
 
-            // if (docs == null) {
-            //     var user = new User({
-            //         name: data.name,
-            //         password: data.password,
-            //         email: data.email,
-            //         phone: data.phone
-            //     });
-            //     user.save(function (err) {
-            //         console.log('save status:', err ? 'failed' : 'success');
-            //         // res.send('success');
-            //         // console.log(name);
-            //         res.sendStatus(201);
-            //         // res.status(201).send('success');
-            //
-            //     });
-            // }
-            if (docs != null) {
+            if (docs == null) {
+                var user = new User({
+                    name: data.name,
+                    password: data.password,
+                    email: data.email,
+                    phone: data.phone
+                });
+                user.save(function (err) {
+                    console.log('save status:', err ? 'failed' : 'success');
+                    res.status(201).send('success');
+
+                });
+            }
+            else if (docs != null) {
                 res.status(409).send('Same name in db.');
             }
             else {
-                res.status(201).send('success');
-                // res.sendStatus(201);
+                res.status(201).send('?');
             }
         });
     }
+    else {
+        res.status(400).send(legal.message);
+    }
+
 
 });
+// router.post('/', function (req, res, next) {
+//     console.log('----usersApi----');
+//
+//     const data = req.body;
+//     const regEmail = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/;
+//     const regPhone = /^(\+86)?(1[0-9]{10})$/;
+//     console.log(regEmail.test(data.email));
+//     console.log(regPhone.test(data.phone) + 'Phone');
+//     if (data.name == '' || data.password == '' || data.email == '' || data.phone == '') {
+//         console.log(data);
+//         console.log('--data is null--');
+//         res.status(400).send('Please finish the form');
+//     }
+//     else if (data.phone.length != 11 || regPhone.test(data.phone) == false) {
+//         res.status(400).send('The phone number is error');
+//     }
+//     else if (regEmail.test(data.email) == false) {
+//         res.status(400).send('The email is error');
+//     }
+//
+//     else {
+//         User.findOne({name: data.name}, function (err, docs) {
+//             if (err) return next(err);
+//
+//             if (docs == null) {
+//                 var user = new User({
+//                     name: data.name,
+//                     password: data.password,
+//                     email: data.email,
+//                     phone: data.phone
+//                 });
+//                 user.save(function (err) {
+//                     console.log('save status:', err ? 'failed' : 'success');
+//                     res.status(201).send('success');
+//
+//                 });
+//             }
+//             else if (docs != null) {
+//                 res.status(409).send('Same name in db.');
+//             }
+//             else {
+//                 res.status(201).send('?');
+//             }
+//         });
+//     }
+//
+// });
 
 
 export default router;
