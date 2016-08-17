8f256883403bd88ec8016f324a6222908513aed8
Leonard LiuTao <364125168@qq.com>
2016-08-17 20:53:56 +0800

    register.jsx finish80%

--- app/api/userApi.js
+++ app/api/userApi.js
@@ -37,7 +37,7 @@ function isUserInformationLegal(data) {
         // res.status(400).send('The phone number is error');
         return {legal: false, message: 'The phone number is error'};
     }
-    else return true;
+    else return {legal: true, message: 'legal is true'};
 }
 
 
@@ -48,7 +48,8 @@ router.post('/', function (req, res, next) {
     console.log('data:' + data.name);
     const legal = isUserInformationLegal(data);
     console.log('legal:' + legal);
-    if (legal == true) {
+    console.log(legal.message);
+    if (legal.legal == true) {
         User.findOne({name: data.name}, function (err, docs) {
             if (err) return next(err);
 
@@ -61,11 +62,12 @@ router.post('/', function (req, res, next) {
                 });
                 user.save(function (err) {
                     console.log('save status:', err ? 'failed' : 'success');
-                    res.status(201).send('success');
+                    res.status(201).send('register success');
 
                 });
             }
             else if (docs != null) {
+
                 res.status(409).send('Same name in db.');
             }
             else {
@@ -79,53 +81,6 @@ router.post('/', function (req, res, next) {
 
 
 });
-// router.post('/', function (req, res, next) {
-//     console.log('----usersApi----');
-//
-//     const data = req.body;
-//     const regEmail = /^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$/;
-//     const regPhone = /^(\+86)?(1[0-9]{10})$/;
-//     console.log(regEmail.test(data.email));
-//     console.log(regPhone.test(data.phone) + 'Phone');
-//     if (data.name == '' || data.password == '' || data.email == '' || data.phone == '') {
-//         console.log(data);
-//         console.log('--data is null--');
-//         res.status(400).send('Please finish the form');
-//     }
-//     else if (data.phone.length != 11 || regPhone.test(data.phone) == false) {
-//         res.status(400).send('The phone number is error');
-//     }
-//     else if (regEmail.test(data.email) == false) {
-//         res.status(400).send('The email is error');
-//     }
-//
-//     else {
-//         User.findOne({name: data.name}, function (err, docs) {
-//             if (err) return next(err);
-//
-//             if (docs == null) {
-//                 var user = new User({
-//                     name: data.name,
-//                     password: data.password,
-//                     email: data.email,
-//                     phone: data.phone
-//                 });
-//                 user.save(function (err) {
-//                     console.log('save status:', err ? 'failed' : 'success');
-//                     res.status(201).send('success');
-//
-//                 });
-//             }
-//             else if (docs != null) {
-//                 res.status(409).send('Same name in db.');
-//             }
-//             else {
-//                 res.status(201).send('?');
-//             }
-//         });
-//     }
-//
-// });
 
 
 export default router;
